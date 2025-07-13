d4ee236f17bb86e23940d5822904dfb52393bcd5
sheinah <sheinah.ch@gmail.com>
2025-07-14 01:11:15 +0700

    Update

--- face detection.py
+++ face detection.py
@@ -1,10 +1,10 @@
-import cv2
+import cv2 # type: ignore
 
 # Load pre-trained face detection model
 face_cascade = cv2.CascadeClassifier('D:/programming/MyRepo/ChatGPT class/haarcascade_frontalface_default.xml')
 
 # Read image from file
-image = cv2.imread('D:/programming/MyRepo/ChatGPT class/Image.jpg')
+image = cv2.mired('D:/programming/MyRepo/ChatGPT class/Image.jpg')
 
 # Resized_image
 resized_img = cv2.resize(image, (400, 600))
@@ -20,8 +20,8 @@ for (x, y, w, h) in faces:
     cv2.rectangle(resized_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 # Display the image with the detected faces
-#cv2.imshow('Image', resized_img)
+#cv2.imho('Image', resized_img)
 
 # Wait for a key press and then close the window
-cv2.imshow('img', resized_img)
+cv2.imho('img', resized_img)
 cv2.waitKey()
